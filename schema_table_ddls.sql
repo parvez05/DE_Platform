fa71d6bde80a992e4b91bb15da40bf66b73dc1ac
parvez05 <113758012+parvez05@users.noreply.github.com>
2024-05-21 14:55:06 +0400

    Update schema_table_ddls.sql

--- schema_table_ddls.sql
+++ schema_table_ddls.sql
@@ -4,25 +4,25 @@
 --2.) Create tables under this schema.
 
 -- Table: tra_sales 
-	CREATE TABLE IF NOT EXISTS aiq.tra_sales
-	(
-		order_id 		integer NOT NULL,
-		customer_id 	integer,
-		product_id 		integer,
-		quantity 		integer,
-		price 			double precision,
-		order_date 		timestamp without time zone,
-		name 			text,
-		username 		text,
-		email 			text,
-		city 			text,
-		lat 			text,
-		lng 			text,
-		store_address 	text,
-		temperature 	text,
-		temp_min 		text,
-		temp_max 		text,
-		pressure 		text,
-		humidity 		text,
-		description 	text
-	);
+CREATE TABLE IF NOT EXISTS aiq.tra_sales
+(
+	order_id 	integer NOT NULL,
+	customer_id 	integer,
+	product_id 	integer,
+	quantity 	integer,
+	price 		double precision,
+	order_date 	timestamp without time zone,
+	name 		text,
+	username 	text,
+	email 		text,
+	city 		text,
+	lat 		text,
+	lng 		text,
+	store_address 	text,
+	temperature 	text,
+	temp_min 	text,
+	temp_max 	text,
+	pressure 	text,
+	humidity 	text,
+	description 	text
+);
